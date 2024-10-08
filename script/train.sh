13628d821acfceb054c092f78e67dddbfcdd04ae
Xuan Feng（冯旋） <947016508@qq.com>
2024-10-08 20:56:12 +0800

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -2,16 +2,4 @@
 
 echo "Start few-shot test..."
 
-bash train_abb.sh
-
-bash train_chaizi.sh
-
-bash train_csc.sh
-
-bash train_mix.sh
-
-bash train_pinyin.sh
-
-# bash train_xingsi.sh
-
-# bash train_yinsi.sh
+python ./finetune/train.py
